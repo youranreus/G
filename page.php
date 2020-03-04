6d486c63f58e48b1ea3cb09b5306e68c9d4c80fb
youranreus <1208944952@qq.com>
2020-03-04 15:36:33 +0800

    小修补

--- page.php
+++ page.php
@@ -4,7 +4,7 @@
 
 		<div id="page">
 			<div id="page-content">
-				<h2 id="page-content-title"><?php if($this->is('page','About')): ?><img src="<?php $this->options->themeUrl('IMG/about.png'); ?>"><?php endif; ?><?php $this->title();?></h2>
+				<h2 id="page-content-title"><?php if($this->is('page','About')or$this->is('page','about')): ?><img src="<?php $this->options->themeUrl('IMG/about.png'); ?>"><?php endif; ?><?php $this->title();?></h2>
 				<div id="page-content-article">
 					<?php
 					$content = $this->content;
