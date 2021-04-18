d376e7338f5dd3e756ded7552c67b63bab5cac21
季悠然 <youranreus@qq.com>
2021-04-18 20:54:57 +0800

    修复分类页面标题CSS问题

--- archive.php
+++ archive.php
@@ -4,7 +4,7 @@
 
  <div id="article">
    <div id="article-content">
-     <h1 style="background: white;padding: 10px;border-radius: 10px;text-align: center;margin: 1rem 1%;"><?php $this -> archiveTitle(array('category' => _t('分类「%s」下的文章'), 'search' => _t('包含关键字「%s」的文章'), 'tag' => _t('标签 「%s」 下的文章'), 'author' => _t('「%s」 发布的文章')), '', ''); ?></h1>
+     <h1 style="background: white;padding: 10px;border-radius: 10px;text-align: center;margin: 1rem 1%;font-size:20px;"><?php $this -> archiveTitle(array('category' => _t('分类「%s」下的文章'), 'search' => _t('包含关键字「%s」的文章'), 'tag' => _t('标签 「%s」 下的文章'), 'author' => _t('「%s」 发布的文章')), '', ''); ?></h1>
      <?php  $this->need('IndexSwitcher.php'); ?>
      <div id="pages" class="clear">
        <?php $this->pageLink('更多 >','next'); ?>
