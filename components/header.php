4d00b0c11202184c28ac9a8467c363dfb0a2edea
youranreus <youranreus@qq.com>
2021-09-30 19:43:35 +0800

    feat: finish basic header structure & dns-prefetch

--- components/header.php
+++ components/header.php
@@ -5,6 +5,11 @@
     <meta charset="<?php $this->options->charset(); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" name="viewport">
+    <link rel="dns-prefetch" href="//cdn.jsdelivr.net">
+    <link rel="icon" type="image/png" href="<?php $this->options->favicon(); ?>">
+    <link href="<?php $this->options->favicon(); ?>" rel="icon">
+    <link rel='dns-prefetch' href='//s.w.org'>
+    <link rel="apple-touch-icon-precomposed" href="<?php $this->options->favicon(); ?>">
     <title><?php $this->archiveTitle(array(
             'category'  =>  _t('分类 %s 下的文章'),
             'search'    =>  _t('包含关键字 %s 的文章'),
@@ -32,5 +37,29 @@
         <div id="header-title">
             <h2><?php $this->options->title(); ?></h2>
         </div>
-        
+        <div id="header-content">
+            <div id="header-content-left">
+                <?php $this->options->description(); ?>
+            </div>
+            <div id="header-content-right">
+                <nav>
+                    <a href="<?php Helper::options()->siteUrl() ?>" <?php if ($this->is('index')) : ?> class="nav-focus"<?php endif; ?>>首页</a>
+                    <?php if ($this->options->enableIndexPage): ?>
+                        <a href="<?php Helper::options()->siteUrl() ?><?php if ($this->options->articlePath != '') {
+                            echo $this->options->articlePath;
+                        } else {
+                            echo 'index.php/blog';
+                        } ?>" <?php if ($this->is('archive') or $this->is('post')) : ?> class="nav-focus"<?php endif; ?>>文章</a>
+                    <?php endif; ?>
+                    <?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
+                    <?php while ($pages->next()): ?>
+                        <?php if ($pages->slug == 'links' or strtolower($pages->slug) == 'about' or $pages->fields->showHeader == 1): ?>
+                            <a class="<?php if ($this->is('page', $pages->slug)): ?> nav-focus<?php endif; ?>"
+                               href="<?php $pages->permalink(); ?>"
+                               title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a>
+                        <?php endif; ?>
+                    <?php endwhile; ?>
+                </nav>
+            </div>
+        </div>
     </header>
