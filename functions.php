ef8eeaf703f6b3b091fbaa3a94703b93dec33070
youranreus <youranreus@qq.com>
2021-09-30 14:26:37 +0800

    refactor: some describe mistakes in themeConfig

--- functions.php
+++ functions.php
@@ -9,16 +9,6 @@ function themeConfig($form) {
 
     $cdn = new Typecho_Widget_Helper_Form_Element_Text('cdn', NULL, NULL, _t('是否开启静态资源cdn加速'), _t('填写加速域名或者jsdelivr，留空则使用本地文件'));
     $form->addInput($cdn);
-
-    // $sidebarBlock = new Typecho_Widget_Helper_Form_Element_Checkbox('sidebarBlock',
-    // array('ShowRecentPosts' => _t('显示最新文章'),
-    // 'ShowRecentComments' => _t('显示最近回复'),
-    // 'ShowCategory' => _t('显示分类'),
-    // 'ShowArchive' => _t('显示归档'),
-    // 'ShowOther' => _t('显示其它杂项')),
-    // array('ShowRecentPosts', 'ShowRecentComments', 'ShowCategory', 'ShowArchive', 'ShowOther'), _t('侧边栏显示'));
-
-    // $form->addInput($sidebarBlock->multiMode());
 }
 
 /*
