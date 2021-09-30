d58c5485c8fee5506849f4424763f9ddeea20253
youranreus <youranreus@qq.com>
2021-09-30 14:18:48 +0800

    feat: staticUrl function

--- functions.php
+++ functions.php
@@ -4,20 +4,22 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 require_once("lib/G.class.php");
 
 function themeConfig($form) {
-    $logoUrl = new Typecho_Widget_Helper_Form_Element_Text('logoUrl', NULL, NULL, _t('站点 LOGO 地址'), _t('在这里填入一个图片 URL 地址, 以在网站标题前加上一个 LOGO'));
-    $form->addInput($logoUrl);
+    $favicon = new Typecho_Widget_Helper_Form_Element_Text('favicon', NULL, NULL, _t('站点 LOGO 地址'), _t('在这里填入一个图片 URL 地址, 以在网站标题前加上一个 LOGO'));
+    $form->addInput($favicon);
 
-    $sidebarBlock = new Typecho_Widget_Helper_Form_Element_Checkbox('sidebarBlock',
-    array('ShowRecentPosts' => _t('显示最新文章'),
-    'ShowRecentComments' => _t('显示最近回复'),
-    'ShowCategory' => _t('显示分类'),
-    'ShowArchive' => _t('显示归档'),
-    'ShowOther' => _t('显示其它杂项')),
-    array('ShowRecentPosts', 'ShowRecentComments', 'ShowCategory', 'ShowArchive', 'ShowOther'), _t('侧边栏显示'));
+    $cdn = new Typecho_Widget_Helper_Form_Element_Text('cdn', NULL, NULL, _t('是否开启静态资源cdn加速'), _t('填写加速域名或者jsdelivr，留空则使用本地文件'));
+    $form->addInput($cdn);
 
-    $form->addInput($sidebarBlock->multiMode());
+    // $sidebarBlock = new Typecho_Widget_Helper_Form_Element_Checkbox('sidebarBlock',
+    // array('ShowRecentPosts' => _t('显示最新文章'),
+    // 'ShowRecentComments' => _t('显示最近回复'),
+    // 'ShowCategory' => _t('显示分类'),
+    // 'ShowArchive' => _t('显示归档'),
+    // 'ShowOther' => _t('显示其它杂项')),
+    // array('ShowRecentPosts', 'ShowRecentComments', 'ShowCategory', 'ShowArchive', 'ShowOther'), _t('侧边栏显示'));
+
+    // $form->addInput($sidebarBlock->multiMode());
 }
-
 
 /*
 function themeFields($layout) {
@@ -25,3 +27,5 @@ function themeFields($layout) {
     $layout->addItem($logoUrl);
 }
 */
+
+G::init();
--- lib/G.class.php
+++ lib/G.class.php
@@ -4,4 +4,34 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
 class G {
     public static $version = "3.0";
+    public static $config = [
+        'favicon'=>'',
+        'cdn'=>'',
+    ];
+
+    public static function init()
+    {
+        $options = Helper::options();
+        $keys = array_keys(self::$config);
+        foreach ($keys as $key) {
+            if(!empty($options->{$key})){
+                self::$config[$key] = $options->{$key};
+            }
+        }
+    }
+
+    /**
+     * 获取静态资源路径
+     */
+    public static function staticUrl($path)
+    {
+        if(self::$config['cdn'] == 'local' || self::$config['cdn'] == '' || self::$config['cdn'] == 'jsdelivr')
+        {
+            return Helper::options()->themeUrl($path);
+        }
+        
+        return self::$config['cdn'].$path;
+    }
+
+    
 }
