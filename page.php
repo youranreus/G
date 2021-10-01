83078869f4bd1ebfd4214f70417d2e54c38597b3
季悠然 <youranreus@qq.com>
2021-10-01 18:25:06 +0800

    fix: require error in page.php and post.php

--- page.php
+++ page.php
@@ -1,5 +1,5 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
-<?php $this->need('header.php'); ?>
+<?php $this->need('components/header.php'); ?>
 
 <div class="col-mb-12 col-8" id="main" role="main">
     <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
@@ -8,8 +8,8 @@
             <?php $this->content(); ?>
         </div>
     </article>
-    <?php $this->need('comments.php'); ?>
+    <?php $this->need('components/comments.php'); ?>
 </div><!-- end #main-->
 
-<?php $this->need('sidebar.php'); ?>
-<?php $this->need('footer.php'); ?>
+<?php $this->need('components/sidebar.php'); ?>
+<?php $this->need('components/footer.php'); ?>
--- post.php
+++ post.php
@@ -1,5 +1,5 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
-<?php $this->need('header.php'); ?>
+<?php $this->need('components/header.php'); ?>
 
 <div class="col-mb-12 col-8" id="main" role="main">
     <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
@@ -15,7 +15,7 @@
         <p itemprop="keywords" class="tags"><?php _e('标签: '); ?><?php $this->tags(', ', true, 'none'); ?></p>
     </article>
 
-    <?php $this->need('comments.php'); ?>
+    <?php $this->need('components/comments.php'); ?>
 
     <ul class="post-near">
         <li>上一篇: <?php $this->thePrev('%s','没有了'); ?></li>
@@ -23,5 +23,5 @@
     </ul>
 </div><!-- end #main-->
 
-<?php $this->need('sidebar.php'); ?>
-<?php $this->need('footer.php'); ?>
+<?php $this->need('components/sidebar.php'); ?>
+<?php $this->need('components/footer.php'); ?>
