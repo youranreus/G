b040142725c64e42907ca0845a39df7d5b7c4027
季悠然 <youranreus@qq.com>
2021-10-04 13:11:32 +0800

    feat: finish footer nav

--- components/footer.php
+++ components/footer.php
@@ -1,7 +1,22 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 
     <footer>
-        <p class="clear">
+        <div id="footer-content">
+            <div id="footer-sponsor">
+                <p>这里是一些图片</p>
+            </div>
+            <nav id="footer-nav">
+				<a href="<?php Helper::options()->siteUrl()?>" <?php if ($this->is('index')) : ?> class="nav-focus"<?php endif; ?>>首页</a>
+				<?php if ($this->options->enableIndexPage): ?>
+						<a href="<?php echo G::getArticlePath(); ?>" <?php if ($this->is('archive') or $this->is('post')) : ?> class="nav-focus"<?php endif; ?>>文章</a>
+				<?php endif; ?>
+				<?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
+                <?php while($pages->next()): ?>
+                    <a href="<?php $pages->permalink(); ?>" <?php if ($this->is('page', $pages->slug)): ?>class="nav-focus"<?php endif; ?> title="<?php $pages->title(); ?>"><?php $pages->title(); ?></a>
+                <?php endwhile; ?>
+			</nav>
+        </div>
+        <p id="footer-meta" class="clear">
             <span class="left"><a class="icp" href="http://beian.miit.gov.cn"><?php echo G::getICP(); ?></a></span>
             <span class="right"><?php echo $this->options->buildYear." - ".date('Y'); ?> &copy <?php $this->options->title(); ?></span>
         </p>
