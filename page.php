63f2b71931a63d676f3af21587089389a28ca688
youranreus <youranreus@qq.com>
2021-10-20 09:07:10 +0800

    fixed: html structure mistake in page.php

--- page.php
+++ page.php
@@ -4,7 +4,9 @@
 <div id="page" class="PAP" role="main">
     <article itemscope itemtype="http://schema.org/BlogPosting">
         <div id="page-banner" class="PAP-banner">
-            <h2 itemprop="name headline"><?php $this->title() ?></h2>
+            <div>
+                <h2 itemprop="name headline"><?php $this->title() ?></h2>
+            </div>
         </div>
         <div id="page-content" class="PAP-content" itemprop="articleBody">
             <?php $this->content(); ?>
