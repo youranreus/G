1ae72a1d73e2b6fb22d7130e70749a9b0ea6ff42
youranreus <youranreus@qq.com>
2021-11-06 11:57:51 +0800

    fix: 修复文章页头部背景不兼容mirage的问题

--- post.php
+++ post.php
@@ -3,8 +3,8 @@
 
 <div id="post" class="PAP" role="main">
     <article itemscope itemtype="http://schema.org/BlogPosting">
-        <div id="post-banner" class="PAP-banner <?php $img = $this->fields->imgurl; if($img != '') echo 'PAP-IMG-Banner'; ?>">
-            <?php $img = $this->fields->imgurl; if($img != ''): ?>
+        <div id="post-banner" class="PAP-banner <?php $img = G::getArticleFieldsBanner($this); if($img != 'none') echo 'PAP-IMG-Banner'; ?>">
+            <?php $img = G::getArticleFieldsBanner($this); if($img != 'none'): ?>
                 <div class="PAP-banner-background" style="background-image: url('<?php echo $img; ?>');"></div>
                 <div class="PAP-banner-mask"></div>
             <?php endif; ?>
