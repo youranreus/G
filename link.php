27a038012cef6ee651c97ea1f773fc72e2962b1d
youranreus <youranreus@qq.com>
2021-11-06 16:06:59 +0800

    feat: 友情链接页面

---
+++ link.php
@@ -0,0 +1,48 @@
+<?php
+/**
+* 友情链接
+*
+* @package custom
+*/
+$this -> need('components/header.php');
+?>
+
+<div class="PAP" id="link">
+	<article itemscope itemtype="http://schema.org/BlogPosting">
+        <div id="post-banner" class="PAP-banner <?php $img = G::getArticleFieldsBanner($this); if($img != 'none') echo 'PAP-IMG-Banner'; ?>">
+            <?php $img = G::getArticleFieldsBanner($this); if($img != 'none'): ?>
+                <div class="PAP-banner-background" style="background-image: url('<?php echo $img; ?>');"></div>
+                <div class="PAP-banner-mask"></div>
+            <?php endif; ?>
+            <div>
+                <h2 itemprop="name headline"><?php $this->title() ?></h2>
+            </div>
+        </div>
+        <div class="post-content PAP-content" itemprop="articleBody">
+            <?php echo G::analyzeContent($this->content); ?>
+        </div>
+    </article>
+</div>
+
+<div id="link-list">
+	<?php if (isset($this->options->plugins['activated']['Links'])) : ?>
+		<?php 
+			Links_Plugin::output('
+				<a target="_blank" href="{url}" class="link-wrap">
+					<div class="link-item">
+						<img src="{image}" alt="{name}"/>
+						<div class="link-item-content">
+							<h4>{name}</h4>
+							<p>#{sort}</p>
+						</div>
+						<div class="link-item-m-content">
+							<span>{name}</span>
+						</div>
+					</div>
+				</a>', 0); 
+		?>
+	<?php endif; ?>
+</div>
+
+
+<?php $this->need('components/footer.php'); ?>
