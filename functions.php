280a6307e78b794526169a955e646083033e20b0
youranreus <youranreus@qq.com>
2021-11-07 11:10:15 +0800

    feat: 更新备份功能

--- functions.php
+++ functions.php
@@ -76,6 +76,82 @@ function themeConfig($form) {
 
     $advanceSetting = new Typecho_Widget_Helper_Form_Element_Textarea('advanceSetting', NULL, NULL, _t('高级设置'), _t('看着就很高级'));
     $form->addInput($advanceSetting);
+
+    $db = Typecho_Db::get();
+    $sjdq=$db->fetchRow($db->select()->from ('table.options')->where ('name = ?', 'theme:G'));
+    $ysj = $sjdq['value'];
+    if(isset($_POST['type']))
+    {
+      if($_POST["type"]=="备份模板数据")
+      {
+        if($db->fetchRow($db->select()->from ('table.options')->where ('name = ?', 'theme:Gbf')))
+        {
+          $update = $db->update('table.options')->rows(array('value'=>$ysj))->where('name = ?', 'theme:Gbf');
+          $updateRows= $db->query($update);
+          echo '<div class="tongzhi">备份已更新，请等待自动刷新！如果等不到请点击';
+          ?>
+            <a href="<?php Helper::options()->adminUrl('options-theme.php'); ?>">这里</a></div>
+            <script language="JavaScript">window.setTimeout("location=\'<?php Helper::options()->adminUrl('options-theme.php'); ?>\'", 2500);</script>
+          <?php
+        }
+        else
+        {
+          if($ysj)
+          {
+            $insert = $db->insert('table.options')->rows(array('name' => 'theme:Gbf','user' => '0','value' => $ysj));
+            $insertId = $db->query($insert);
+            echo '<div class="tongzhi">备份完成，请等待自动刷新！如果等不到请点击';
+            ?>
+              <a href="<?php Helper::options()->adminUrl('options-theme.php'); ?>">这里</a></div>
+              <script language="JavaScript">window.setTimeout("location=\'<?php Helper::options()->adminUrl('options-theme.php'); ?>\'", 2500);</script>
+            <?php
+          }
+        }
+      }
+      if($_POST["type"]=="还原模板数据")
+      {
+
+        if($db->fetchRow($db->select()->from ('table.options')->where ('name = ?', 'theme:Gbf')))
+        {
+
+          $sjdub=$db->fetchRow($db->select()->from ('table.options')->where ('name = ?', 'theme:Gbf'));
+          $bsj = $sjdub['value'];
+          $update = $db->update('table.options')->rows(array('value'=>$bsj))->where('name = ?', 'theme:G');
+          $updateRows= $db->query($update);
+          echo '<div class="tongzhi">检测到模板备份数据，恢复完成，请等待自动刷新！如果等不到请点击';
+          ?>
+            <a href="<?php Helper::options()->adminUrl('options-theme.php'); ?>">这里</a></div>
+            <script language="JavaScript">window.setTimeout("location=\'<?php Helper::options()->adminUrl('options-theme.php'); ?>\'", 2000);</script>
+          <?php
+
+        }
+        else
+        {
+
+          echo '<div class="tongzhi">没有模板备份数据，恢复不了哦！</div>';
+
+        }
+      }
+      if($_POST["type"]=="删除备份数据")
+      {
+        if($db->fetchRow($db->select()->from ('table.options')->where ('name = ?', 'theme:Gbf')))
+        {
+          $delete = $db->delete('table.options')->where ('name = ?', 'theme:Gbf');
+          $deletedRows = $db->query($delete);
+          echo '<div class="tongzhi">删除成功，请等待自动刷新，如果等不到请点击';
+          ?>
+          <a href="<?php Helper::options()->adminUrl('options-theme.php'); ?>">这里</a></div>
+          <script language="JavaScript">window.setTimeout("location=\'<?php Helper::options()->adminUrl('options-theme.php'); ?>\'", 2500);</script>
+          <?php
+        }
+        else
+        {
+          echo '<div class="tongzhi">不用删了！备份不存在！！！</div>';
+        }
+      }
+    }
+    echo '<div id="backup"><form class="protected Data-backup" action="?Gbf" method="post"><h4>数据备份</h4>
+    <input type="submit" name="type" class="btn btn-s" value="备份模板数据" />&nbsp;&nbsp;<input type="submit" name="type" class="btn btn-s" value="还原模板数据" />&nbsp;&nbsp;<input type="submit" name="type" class="btn btn-s" value="删除备份数据" /></form></div>';
 }
 
 
