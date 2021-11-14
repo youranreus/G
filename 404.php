ab467cc837f5358aad9e833177bb300fbc4207ee
youranreus <youranreus@qq.com>
2021-11-14 10:34:42 +0800

    update: 更新404页面

---
+++ 404.php
@@ -0,0 +1,6 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+<?php $this->need('components/header.php'); ?>
+<div id="page404" class="PAP" role="main">
+    <h2 style="font-weight: 200; font-size: 4rem; opacity: .3;text-align: center;margin: 15rem 0;">没有找到呢</h2>
+</div>
+<?php $this->need('components/footer.php'); ?>
