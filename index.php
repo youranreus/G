0e842550fe0e93fa2d6d6b09f4ea6b8f722abe24
youranreus <youranreus@qq.com>
2021-11-15 14:07:54 +0800

    update: version 3.2.4

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  *
  * @package G
  * @author 季悠然
- * @version 3.2.3
+ * @version 3.2.4
  * @link https://季悠然.space
  */
 
--- lib/G.class.php
+++ lib/G.class.php
@@ -9,7 +9,7 @@ class G {
      *
      * @var string
      */
-    public static $version = "3.2.3";
+    public static $version = "3.2.4";
 
     /**
      * 主题配置
