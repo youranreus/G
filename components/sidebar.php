b05f81d7b35c4a97c059234937f9a1476dbc83a3
youranreus <youranreus@qq.com>
2022-04-15 13:16:54 +0800

    feat: 兼容旧配置

--- components/sidebar.php
+++ components/sidebar.php
@@ -28,43 +28,71 @@
 
     <div id="widgets">
         <?php 
-            $widgets = json_decode($this->options->customWidgets)->widgets;
-            foreach($widgets as $w)
+            if($this->options->customWidgets != '') 
             {
-                if($w->type == "photo")
+                $widgets = json_decode($this->options->customWidgets);
+                if (isset($widgets->widgets))
                 {
-                    echo '
-                        <div class="widget widget-photo '.($w->size == 'large' ? 'large' : '').'" style="background-image:url('.$w->url.')">
-                            <div>
-                                <p>'.$w->desc.'</p>
+                    foreach($widgets->widgets as $w)
+                    {
+                        if($w->type == "photo")
+                        {
+                            echo '
+                                <div class="widget widget-photo '.($w->size == 'large' ? 'large' : '').'" style="background-image:url('.$w->url.')">
+                                    <div>
+                                        <p>'.$w->desc.'</p>
+                                    </div>
+                                </div>
+                            ';
+                        }
+                        else if ($w->type == "cate")
+                        {
+                            echo '
+                                <div class="widget category-list '.($w->size == 'large' ? 'large' : '').'">
+                                    <div class="category-content">
+                                        <li><a onclick="toggleSidebar()" href="'.$w->content[0]->url.'">'.$w->content[0]->name.'</a></li>
+                                        <li><a onclick="toggleSidebar()" href="'.$w->content[1]->url.'">'.$w->content[1]->name.'</a></li>
+                                        <li><a onclick="toggleSidebar()" href="'.$w->content[2]->url.'">'.$w->content[2]->name.'</a></li>
+                                    </div>
+                                </div>
+                            ';
+                        }
+                        else if ($w->type == "video")
+                        {
+                            echo '
+                            <div class="widget widget-photo '.($w->size == 'large' ? 'large' : '').'">
+                                <video class="widget-video" loop muted autoplay src="'.$w->url.'"></video>
+                                <div>
+                                    <p>'.$w->desc.'</p>
+                                </div>
                             </div>
-                        </div>
-                    ';
+                            ';
+                        }
+                    }
                 }
-                else if ($w->type == "cate")
+                else
                 {
-                    echo '
-                        <div class="widget category-list '.($w->size == 'large' ? 'large' : '').'">
-                            <div class="category-content">
-                                <li><a onclick="toggleSidebar()" href="'.$w->content[0]->url.'">'.$w->content[0]->name.'</a></li>
-                                <li><a onclick="toggleSidebar()" href="'.$w->content[1]->url.'">'.$w->content[1]->name.'</a></li>
-                                <li><a onclick="toggleSidebar()" href="'.$w->content[2]->url.'">'.$w->content[2]->name.'</a></li>
-                            </div>
-                        </div>
-                    ';
-                }
-                else if ($w->type == "video")
-                {
-                    echo '
-                    <div class="widget widget-photo '.($w->size == 'large' ? 'large' : '').'">
-                        <video class="widget-video" loop muted autoplay src="'.$w->url.'"></video>
-                        <div>
-                            <p>'.$w->desc.'</p>
-                        </div>
-                    </div>
-                    ';
+                    echo '<script>alert("widgets配置出错");</script>';
                 }
             }
+            else
+            { ?>
+                <div class="widget category-list" style="<?php if ($this->options->profilePhoto == ''): ?>width: 15rem;<?php endif; ?>">
+                    <div class="category-content">
+                        <?php $this->widget('Widget_Metas_Category_List')->parse('<li><a onclick="toggleSidebar()" href="{permalink}">{name}</a></li>'); ?>
+                    </div>
+                </div>
+                <?php if ($this->options->profilePhoto != ''): ?>
+                    <div class="widget widget-photo" style="background-image:url(<?php echo $this->options->profilePhoto; ?>)">
+                        <?php if ($this->options->profileVideo != ''): ?>
+                            <video class="widget-video" loop muted autoplay src="<?php echo $this->options->profileVideo; ?>" poster="<?php echo $this->options->profilePhoto; ?>"></video>
+                        <?php endif; ?>
+                        <div>
+                            <p><?php echo $this->options->profilePhotoDes; ?></p>
+                        </div>
+                    </div>
+                <?php endif; ?>
+            <?php }
         ?>
     </div>
 </div>
