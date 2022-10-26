072a36f3512080b2db1cb431c4c783b5a325f98c
youranreus <youranreus@qq.com>
2022-10-26 09:05:25 +0800

    fix: 修复自动夜间模式加载逻辑

--- components/header.php
+++ components/header.php
@@ -36,7 +36,7 @@ if (isset($_POST['DYLM'])) {
     <?php endif; ?>
     <link rel="preload" href="<?php echo G::staticUrl('static/js/spotlight.bundle.js'); ?>" as="script">
     <link rel="stylesheet" href="<?php echo G::staticUrl('static/css/G.css'); ?>?v=<?php echo $devTag; ?>">
-    <link rel="stylesheet" href="<?php echo G::staticUrl("static/css/dark$minInfix.css?v=$devTag"); ?>" title="dark">
+    <link rel="stylesheet alternate" href="<?php echo G::staticUrl("static/css/dark$minInfix.css?v=$devTag"); ?>" title="dark" disabled>
 
     <style>
         /* 设置自定义背景[颜色/图片] */
