95d47b9263f0be22aebfc4008197fce79938464e
youranreus <youranreus@qq.com>
2022-11-13 18:05:34 +0800

    fix: tiny fixes

--- components/header.php
+++ components/header.php
@@ -34,7 +34,6 @@ if (isset($_POST['DYLM'])) {
     <?php if($this->options->enableKatex == 1): ?>
         <link rel="stylesheet" href="<?php echo G::staticUrl('static/css/katex.min.css'); ?>">
     <?php endif; ?>
-    <link rel="preload" href="<?php echo G::staticUrl('static/js/spotlight.bundle.js'); ?>" as="script">
     <link rel="stylesheet" href="<?php echo G::staticUrl('static/css/G.css'); ?>?v=<?php echo $devTag; ?>">
     <link rel="stylesheet alternate" href="<?php echo G::staticUrl("static/css/dark$minInfix.css?v=$devTag"); ?>" title="dark" disabled>
 
